21523c34a4aea4c890ede93bfde03c9999a06e9e
Pid Zwei <153316008+proc-2@users.noreply.github.com>
2024-01-01 20:40:18 +0100

    :books: docs: Improve docstrings integration.

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
     'sphinx_automodapi.automodapi',
     'sphinx_multiversion',
